7f1b19057f7e361b19f86ff5540bd977281ac5fa
Charles <charles.andref@gmail.com>
2017-11-30 14:17:18 -0500

    Modele de lien vers la BD

---
+++ GymXpressSolution/GymXpress/Models/DbContext.cs
@@ -0,0 +1,35 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace GymXpress.Models
+{
+    public class DbContext
+    {
+        protected static MySqlConnection cnx;
+        public DbContext()
+        {
+            string cs = ConfigurationManager.ConnectionStrings["MySQLConnection"].ConnectionString;
+
+            try
+            {
+                cnx = new MySqlConnection(cs);
+                cnx.Open();
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine("Error: {0}", ex.ToString());
+            }
+        }
+        public void Dispose()
+        {
+            if (cnx != null)
+            {
+                cnx.Close();
+            }
+        }
+    }
+}
---
+++ GymXpressSolution/GymXpress/Models/Idal.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GymXpress.Models
+{
+    interface Idal : IDisposable
+    {
+        void CreerCompte(int role, string courriel, string motPasse);
+    }
+}
