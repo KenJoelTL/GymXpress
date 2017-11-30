7740d7b3cf5edd896d6600988d51d26037e01712
Charles <charles.andref@gmail.com>
2017-11-30 12:30:12 -0500

    debut du projet

---
+++ GymXpressSolution/GymXpress/Models/Compte.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GymXpress.Models
+{
+    public class Compte
+    {
+        private int idCompte;
+        private int role;
+        private string courriel;
+        private string motPasse;
+
+        public int IdCompte
+        {
+            get { return idCompte; }
+            set { idCompte = value; }
+        }
+
+        public int Role
+        {
+            get { return role; }
+            set { role = value; }
+        }        
+
+        public string Courriel
+        {
+            get { return courriel; }
+            set { courriel = value; }
+        }
+        
+        public string MotPasse
+        {
+            get { return motPasse; }
+            set { motPasse = value; }
+        }
+
+        
+
+
+    }
+}
