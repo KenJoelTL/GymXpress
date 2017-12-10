0b7ecc02558c6986173857cec6b50ea8d2eb9dd0
Charles <charles.andref@gmail.com>
2017-12-10 06:53:01 -0500

    Correctif DAL + mise en commentaire du filtre dans Global.asax

--- GymXpressSolution/GymXpress/Global.asax.cs
+++ GymXpressSolution/GymXpress/Global.asax.cs
@@ -13,8 +13,10 @@ namespace GymXpress
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
+          //  FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
-            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+
+
         }
     }
 }
