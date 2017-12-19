b4dc917f7560b418fed535ab47d4a3b3d0f7b876
KenJoelTL <joellutumba@hotmail.com>
2017-12-18 22:47:32 -0500

    Ajout de quelque test de planControlleur

--- GymXpressSolution/GymXpress.Tests/PlanControllerTest.cs
+++ GymXpressSolution/GymXpress.Tests/PlanControllerTest.cs
@@ -2,6 +2,8 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using GymXpress.Controllers;
 using System.Web.Mvc;
+using GymXpress.Models;
+using System.Linq;
 
 namespace GymXpress.Tests {
     [TestClass]
@@ -20,10 +22,42 @@ namespace GymXpress.Tests {
         }
 
         [TestMethod]
+        public void CreationPostTest() {
+            Plan plan = new Plan() { Nom = "Plan de test", IdEntraineur = 1, Description = "Ceci est un plan de test", IdCompte = 1 };
+            var result = planController.Create(plan) as RedirectToRouteResult;
+            Assert.AreEqual("Index", result.RouteValues["action"]);
+        }
+        
+        [TestMethod]
+        public void EditTest() {
+            var param = 1;
+            var result = planController.Edit(param) as ViewResult;
+            Assert.AreEqual("", result.ViewName);
+            using (Dal dal = new Dal()) {
+                Plan plan = dal.ObtenirTousLesPlans().FirstOrDefault();
+                var result2 = planController.Edit(plan.IdPlan) as ViewResult;
+                Assert.AreEqual(plan.IdPlan,((Plan)result2.Model).IdPlan);
+            }
+            param = -1;
+            var result3 = planController.Edit(param) as RedirectToRouteResult;
+            Assert.AreEqual("Index", result3.RouteValues["action"]);
+        }
+        /*
+        [TestMethod]
+        public void EditPostTest() {
+            Plan param = new Plan();
+            var result = planController.Edit(param) as ViewResult;
+            Assert.IsNull(result);
+            var result2 = planController.Edit(param) as RedirectToRouteResult;
+            Assert.AreEqual("Index", result2.RouteValues["action"]);
+        }
+        */
+        /*
+        [TestMethod]
         public void TestRenvoieVueLorsqueNon() {
             var result = planController.Index() as ViewResult;
             Assert.AreEqual("", result.ViewName);
-        }
+        }*/
 
 
 
